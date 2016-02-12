85532559c4d3f51a3db725d522180225b2588e46
abraham28s <ab.soto@hotmail.com>
2016-02-12 14:58:48 -0600

    Initial commit

---
+++ core/src/mx/itesm/skullfighter/Principal.java
@@ -0,0 +1,7 @@
+package mx.itesm.skullfighter;
+
+/**
+ * Created by abrahamsoto on 12/02/16.
+ */
+public class Principal {
+}
---
+++ desktop/src/mx/itesm/skullfighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.skullfighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.skullfighter.SkullFighter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SkullFighter(), config);
+	}
+}
