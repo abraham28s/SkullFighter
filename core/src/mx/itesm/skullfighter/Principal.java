71aae40f453dae281ab234b4c1b7e883eb5040ad
abraham28s <ab.soto@hotmail.com>
2016-02-12 15:03:58 -0600

    Prueba commit

--- core/src/mx/itesm/skullfighter/Principal.java
+++ core/src/mx/itesm/skullfighter/Principal.java
@@ -4,4 +4,12 @@ package mx.itesm.skullfighter;
  * Created by abrahamsoto on 12/02/16.
  */
 public class Principal {
+    public static final float ANCHO_MUNDO = 1280;
+    public static final float ALTO_MUNDO = 720;
+    private String hola;
+
+    @Override
+    public void create () {
+        setScreen(new PantallaMenu(this));
+    }
 }
