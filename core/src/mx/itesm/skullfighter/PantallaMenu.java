02fb4d55420966060fd1b337f81d24cae14e0157
abraham28s <ab.soto@hotmail.com>
2016-02-12 15:32:03 -0600

    Prueba commit

---
+++ core/src/mx/itesm/skullfighter/PantallaMenu.java
@@ -0,0 +1,11 @@
+package mx.itesm.skullfighter;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by abrahamsoto on 12/02/16.
+ */
+public class PantallaMenu implements Screen {
+    public PantallaMenu(Principal principal) {
+    }
+}
--- core/src/mx/itesm/skullfighter/Principal.java
+++ core/src/mx/itesm/skullfighter/Principal.java
@@ -1,15 +1,17 @@
 package mx.itesm.skullfighter;
 
+import com.badlogic.gdx.Game;
+
 /**
  * Created by abrahamsoto on 12/02/16.
  */
-public class Principal {
+public class Principal extends Game{
     public static final float ANCHO_MUNDO = 1280;
     public static final float ALTO_MUNDO = 720;
-    private String hola; //adios
+    private String hola;
 
 
-
+    @Override
     public void create () {
         setScreen(new PantallaMenu(this));
     }
