64632895ba70d77dca6952a1ed2dafbc86a532a2
abraham28s <ab.soto@hotmail.com>
2016-02-16 14:55:58 -0600

    Se agrego manejador de pantallas

--- core/src/mx/itesm/skullfighter/PantallaMenu.java
+++ core/src/mx/itesm/skullfighter/PantallaMenu.java
@@ -1,17 +1,49 @@
 package mx.itesm.skullfighter;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by abrahamsoto on 12/02/16.
  */
 public class PantallaMenu implements Screen {
+
+    private final Principal principal;
+    private OrthographicCamera camara;
+    private Viewport vista;
+
+    private SpriteBatch batch;
+
+    //fondo
+    private Fondo fondo;
+    private Texture texturaFondo;
+
+    //boton play
+    private BotonMenu btnPlay;
+    private Texture texturaBtnPlay;
     public PantallaMenu(Principal principal) {
+        this.principal = principal;
     }
 
     @Override
     public void show() {
+        //Crear camara y vista
+        camara = new OrthographicCamera(Principal.ANCHO_MUNDO, Principal.ALTO_MUNDO);
+        camara.position.set(Principal.ANCHO_MUNDO / 2, Principal.ALTO_MUNDO / 2, 0);
+        camara.update();
+        vista = new StretchViewport(Principal.ANCHO_MUNDO,Principal.ALTO_MUNDO,camara);
 
+
+        cargarTexturas();
+        fondo = new Fondo(texturaFondo);
+        btnPlay = new BotonMenu(texturaBtnPlay);
+        btnPlay.setPosicion(Principal.ANCHO_MUNDO/2,Principal.ALTO_MUNDO-(Principal.ALTO_MUNDO/4));
+
+        batch = new SpriteBatch();
     }
 
     @Override
